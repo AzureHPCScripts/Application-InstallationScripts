59d7e399e1d360e521e0bd74a02542c881a5e26f
AzureHPCScripts <123459094+AzureHPCScripts@users.noreply.github.com>
2023-02-24 16:36:26 +0530

    Create install.bat

---
+++ AltairApps./Windows./NFX./install.bat
@@ -0,0 +1,36 @@
+#Installation script for Altair NFX
+#------------------------------------------------------------------
+
+set -e
+
+#declaring the package downloaded and installation directory for the application installation
+
+set DOWNLOAD_DIR=C:\Users\%username%\Desktop\altair
+
+set PACKAGE=hwCFDSolvers2022.1_win64.exe
+
+set PACKAGE_LM=altair_licensing_15.1.win64_x64.exe
+
+echo "Downloaded directory: %DOWNLOAD_DIR%"
+
+echo "PACKAGE Name: %PACKAGE%"
+
+echo "PACKAGE_LM Name: $PACKAGE_LM%"
+
+
+
+cd %DOWNLOAD_DIR%
+
+
+echo "Installing NFX Application"
+
+.\%PACKAGE% -i silent -DACCEPT_EULA=YES
+
+echo "NFX installed successfully"
+
+
+echo "Installing  License Manager"
+
+.\%PACKAGE_LM% -i silent -DACCEPT_EULA=YES
+
+echo "License manager is completely installed"
